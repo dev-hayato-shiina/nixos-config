74fa7b2c5c1f506dd999885d1847d6c447028116
dev-hayato-shiina <dev.hayato.shiina@gmail.com>
2026-04-04 00:09:12 +0900

    fix: setup_ssh_key.sh の更新

--- setup_ssh_key.sh
+++ setup_ssh_key.sh
@@ -4,9 +4,11 @@ SSH_DIR="$HOME/.ssh"
 
 KEY_PATH_DEV_HAYATO_SHIINA="${SSH_DIR}/dev-hayato-shiina-key"
 KEY_PATH_SHEENA_DEVELOP="${SSH_DIR}/sheena-develop-key"
+KEY_PATH_SSH_HAYATO_SHIINA="${SSH_DIR}/ssh-hayato-shiina"
 
 BW_ITEM_ID_DEV_HAYATO_SHIINA="24d853e0-895b-4c88-835c-b3ff01315dc6"
 BW_ITEM_ID_SHEENA_DEVELOP="92ade4ad-1253-4345-aaf6-b3ff0131ce58"
+BW_ITEM_ID_SSH_HAYATO_SHIINA="f1b229e8-f064-4570-8ce2-b42100e37de0"
 
 # Bitwarden セッション確認
 if [[ -z "$BW_SESSION" ]]; then
@@ -37,9 +39,10 @@ fi
 
 # 配置
 mkdir -p "$SSH_DIR"
+chmod 700 "$SSH_DIR"
+
 echo "$PRIVATE_KEY" > "$KEY_PATH_DEV_HAYATO_SHIINA"
 chmod 600 "$KEY_PATH_DEV_HAYATO_SHIINA"
-chmod 700 "$SSH_DIR"
 echo "秘密鍵を配置しました: $KEY_PATH_DEV_HAYATO_SHIINA"
 
 # 動作確認
@@ -62,10 +65,8 @@ if [[ -z "$PRIVATE_KEY" || "$PRIVATE_KEY" == "null" ]]; then
 fi
 
 # 配置
-mkdir -p "$SSH_DIR"
 echo "$PRIVATE_KEY" > "$KEY_PATH_SHEENA_DEVELOP"
 chmod 600 "$KEY_PATH_SHEENA_DEVELOP"
-chmod 700 "$SSH_DIR"
 echo "秘密鍵を配置しました: $KEY_PATH_SHEENA_DEVELOP"
 
 # 動作確認
@@ -75,3 +76,21 @@ ssh -T sheena-develop
 # ssh-keygen -p -f ~/.ssh/sheena-develop-key
 
 # ======================
+
+# === ssh-hayato-shiina ===
+
+# 秘密鍵取得
+PRIVATE_KEY=$(bw get item "$BW_ITEM_ID_SSH_HAYATO_SHIINA" --session "$BW_SESSION" \
+  | jq -r '.notes')
+
+if [[ -z "$PRIVATE_KEY" || "$PRIVATE_KEY" == "null" ]]; then
+  echo "秘密鍵の取得に失敗しました（ssh-hayato-shiina）"
+  exit 1
+fi
+
+# 配置
+echo "$PRIVATE_KEY" > "$KEY_PATH_SSH_HAYATO_SHIINA"
+chmod 600 "$KEY_PATH_SSH_HAYATO_SHIINA"
+echo "秘密鍵を配置しました: $KEY_PATH_SSH_HAYATO_SHIINA"
+
+# =========================
