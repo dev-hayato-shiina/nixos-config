9194c23a5bf418c1bcad769e89ba527ed9d67268
dev-hayato-shiina <dev.hayato.shiina@gmail.com>
2026-04-09 00:22:57 +0900

    fix: Zellij のレイアウト調整

--- modules/hm/zellij/default.nix
+++ modules/hm/zellij/default.nix
@@ -113,11 +113,22 @@ in
 
   xdg.configFile."zellij/layouts/default.kdl".text = ''
     layout {
+        default_tab_template {
+            pane size=1 borderless=true {
+                plugin location="zellij:tab-bar"
+            }
+            children
+            pane size=1 borderless=true {
+                plugin location="zellij:status-bar"
+            }
+        }
         tab {
             pane split_direction="Vertical" {
                 pane split_direction="Horizontal" size="70%" {
-                    pane size="80%"
-                    pane size="20%"
+                    pane stacked=true size="100%" {
+                        pane
+                        pane
+                    }
                 }
                 pane size="30%"
             }
