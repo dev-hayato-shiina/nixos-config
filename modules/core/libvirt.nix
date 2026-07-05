0b0a8ff8149c9f6d019117a79592c1acf6048d28
dev-hayato-shiina <dev.hayato.shiina@gmail.com>
2026-07-05 16:39:12 +0900

    update

--- modules/core/libvirt.nix
+++ modules/core/libvirt.nix
@@ -1,8 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
-
+  environment.systemPackages = with pkgs; [
+    dnsmasq
+  ];
   users.users."hayato-shiina".extraGroups = [
     "libvirtd"
   ];
